58ae528fd2a52e1f1de7bff4875ff96aa7108891
JavierUlisesAmarilla <amarillajavierulises@gmail.com>
2024-06-05 21:19:00 -0300

    update title

--- src/Scene/ResiumScene/ResiumScene.tsx
+++ src/Scene/ResiumScene/ResiumScene.tsx
@@ -4,10 +4,14 @@ import {ResiumViewCube} from './ResiumViewCube'
 import {ResiumWorld} from './ResiumWorld'
 
 
+Cesium.Ion.defaultAccessToken =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0YmZhMWZkYy01ZTZjLTQ2NWYtYTkyNi0yZTk5MjcwMzIwZjMiLCJpZCI6MTIwOTI3LCJpYXQiOjE2NzMzNjU5MDV9.t8oV9N6iImPQNUOQm-T-EE3VKyh65U6pGbv5_m0Kx80'
+
+
 export const ResiumScene = ({
   terrainProvider,
 }: {
-  terrainProvider: Cesium.CesiumTerrainProvider
+  terrainProvider: Cesium.CesiumTerrainProvider;
 }) => {
   return (
     <>
